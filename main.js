63e654aba58be6bbd321b3aea74750fc1dcd4b77
Gabrielpaje <134975502+Gabrielpaje@users.noreply.github.com>
2023-10-09 20:03:09 -0300

    testando o main

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 
 function tocaSomClap () {
-    document.querySelector("#som_tecla_pom").play();
+    document.querySelector("#som_tecla_clap").play();
 }
 
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
